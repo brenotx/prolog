2087890ae7d576c601aa0e0a62e3d1b6873e4d8f
Breno <brenotx@gmail.com>
2013-12-07 19:10:21 -0200

    Traducao da aplicacao para pt-BR

--- exSystem01.pl
+++ exSystem01.pl
@@ -1,67 +1,81 @@
-/* exSystem.pro
+/* exSystem01.pro
   character identification game.  
 
-    start with ?- go.     */
+    start with ?- main.     */
 
-go :- hypothesize(Character),
-      write('I guess that the character is: '),
-      write(Character),
+main :- write('_______________________________________________________________________________'), nl,
+        write('Este é um sistema especialista dos principais personagens da série de TV Chaves')                      , nl,
+        write('Uso: ')                                                                          , nl,
+        write(' go. para iniciar as perguntas. ')                                       , nl, 
+        write(' undo. para resetar suas respostas. ')                                            , nl, nl,
+        write('Para responder, utilize as palavras yes/no OU y/n. '), nl, 
+        write('_______________________________________________________________________________')     , nl,   
+        write(' '),
+        read(Command),
+        command(Command).
+
+command(go) :- go.
+
+go :- 
+      hypothesize(Character, Translate),
+      write('Eu acho que o personagem é: '),
+      write(Translate),
       nl,
       undo.
 
 /* hypotheses to be tested */
-hypothesize(quico)          :- quico, !.
-hypothesize(chaves)         :- chaves, !.
-hypothesize(nhonho)         :- nhonho, !.
-hypothesize(senhor_barriga) :- senhor_barriga, !.
-hypothesize(girafales)      :- girafales, !.
-hypothesize(seu_madruga)    :- seu_madruga, !.
-hypothesize(jaiminho)       :- jaiminho, !.
-hypothesize(chiquinha)      :- chiquinha, !.
-hypothesize(popis)          :- popis, !.
-hypothesize(dona_florinda)  :- dona_florinda, !.
-hypothesize(dona_clotilde)  :- dona_clotilde, !.
+hypothesize(quico, 'Quico')                   :- quico, !.
+hypothesize(chaves, 'Chaves')                 :- chaves, !.
+hypothesize(nhonho, 'Nhonho')                 :- nhonho, !.
+hypothesize(senhor_barriga, 'Senhor Barriga') :- senhor_barriga, !.
+hypothesize(girafales, 'Professor Girafales') :- girafales, !.
+hypothesize(seu_madruga, 'Seu madruga')       :- seu_madruga, !.
+hypothesize(jaiminho, 'Jaiminho')             :- jaiminho, !.
+hypothesize(chiquinha, 'Chiquinha')           :- chiquinha, !.
+hypothesize(popis, 'Popis')                   :- popis, !.
+hypothesize(dona_florinda, 'Dona Florinda')   :- dona_florinda, !.
+hypothesize(dona_clotilde, 'Dona Clotilde')   :- dona_clotilde, !.
 hypothesize(unknown).             /* no diagnosis */
 
 /* character identification rules */
 quico :- boy, 
-         verify(has_multicolored_hat).
+         verify(has_multicolored_hat, 'possui chapéu colorido').
 
 chaves :- boy,
-          verify(lives_in_a_barrel). 
+          verify(lives_in_a_barrel, 'mora em um barril'). 
 
 nhonho :- boy,
-          verify(is_fat). % n precisa!
+          verify(is_fat, 'é gordo').
 
 senhor_barriga :- adult_man,
-                  verify(wears_suit),
-                  verify(is_fat).
+                  verify(wears_suit, 'usa terno'),
+                  verify(is_fat, 'é gordo').
 
 girafales :- adult_man,
-             verify(wears_suit),
-             verify(is_a_teacher).
+             verify(wears_suit, 'usa terno'),
+             verify(is_a_teacher, 'é professor').
 
 seu_madruga :-  adult_man,
-                verify(does_not_pay_rent).
+                verify(does_not_pay_rent, 'nunca paga o aluguel').
                 
 jaiminho :- adult_man,
-            verify(is_a_postman).
+            verify(is_a_postman, 'é um carteiro').
 
 chiquinha :- girl,
-             verify(wear_glasses).  
+             verify(wear_glasses, 'usa óculos').  
 
 popis :- girl, 
-         verify(holds_a_doll). 
+         verify(holds_a_doll, 'sempre está segurando uma boneca'). 
 
 dona_florinda :- adult_woman,
-                 verify(wears_curlers).
+                 verify(wears_curlers, 'usa bobes no cabelo').
 
 dona_clotilde :- adult_woman,
-                 verify(knows_dark_magic).
+                 verify(knows_dark_magic, 'conhece magia negra').
 
 /* classification rules */
-child :- verify(is_child).
-man   :- verify(is_man).
+child :- verify(is_child, 'é crianca').
+man   :- verify(is_man, 'é homem').
 
 adult_man :- not(child),
              man.
@@ -76,9 +90,9 @@ girl :- child,
         not(man).
 
 /* how to ask questions */
-ask(Question) :-
-    write('Does the character have the following attribute: '),
-    write(Question),
+ask(Question, Translate) :-
+    write('O personagem possui o seguinte atributo: '),
+    write(Translate),
     write('? '),
     read(Response),
     nl,
@@ -90,14 +104,14 @@ ask(Question) :-
 :- dynamic yes/1,no/1.
 
 /* How to verify something */
-verify(S) :-
+verify(S, Ptbr) :-
    (yes(S) 
     ->
     true ;
     (no(S)
      ->
      fail ;
-     ask(S))).
+     ask(S, Ptbr))).
 
 /* undo all yes/no assertions */
 undo :- retract(yes(_)),fail. 
